189dcfadbd0361d2236c56bb7a90302b546a9f8e
winkele <nstar7272@mail.ru>
2023-05-13 09:37:51 +0300

    Hided action bar, very small adjusment

--- app/src/main/java/com/example/individualproject/DetailedRecipeActivity.java
+++ app/src/main/java/com/example/individualproject/DetailedRecipeActivity.java
@@ -38,6 +38,7 @@ public class DetailedRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail);
+        getSupportActionBar().hide();
 
         recImage = RecipesActivity.recImage;
         recSteps = RecipesActivity.recSteps;
